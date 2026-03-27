2a1fd4bf4635e2ce5eed40992cbb9e795547964c
Lukas Hanusovsky <lhanusov@redhat.com>
2026-03-27 09:04:45 +0100

    Migration Tool - Updating CommonStatements and WebDriver rewrite, adding ApiUtil and AdminClient rewrite.
    Signed-off-by: Lukas Hanusovsky <lhanusov@redhat.com>

--- tests/migration-util/src/main/java/org/keycloak/test/migration/MigrateTest.java
+++ tests/migration-util/src/main/java/org/keycloak/test/migration/MigrateTest.java
@@ -5,17 +5,13 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Stream;
-
-import static java.lang.Thread.sleep;
 
 public class MigrateTest {
 
@@ -35,6 +31,8 @@ public class MigrateTest {
             PageObjectRewrite.class,
             RunOnServerRewrite.class,
             CommonStatementsRewrite.class,
+            ApiUtilRewrite.class,
+            AdminClientRewrite.class,
             TestRealmRewrite.class
     );
 
